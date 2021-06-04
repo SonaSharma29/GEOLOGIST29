cef20048093b0d462d3d8fc682afc56edab4f947
#SonaBansotra# <83403018+SonaSharma29@users.noreply.github.com>
2021-06-04 15:44:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var ball;
+var stone;
+var iron1;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    ball = new Rubber (900,20,50)
+    stone = new Stone(600,200,200,30);
+    iron1 = new Iron (300,300,100,50)
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    ball.display();
+    stone.display();
+    iron1.display();
+ 
+}
